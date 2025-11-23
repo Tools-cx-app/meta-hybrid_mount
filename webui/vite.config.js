0cb87f22f874b2e3e792252be2a3c6973d560d29
Tools-app <localhost.hutao@gmail.com>
2025-11-23 15:02:25 +0800

    Revert "remove webui"
    This reverts commit e817d967a4d5a6797f588a62edbdc5101285dd5a.

---
+++ webui/vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: './',
+  build: {
+    outDir: '../template/webroot',
+  },
+  plugins: [svelte()],
+})
